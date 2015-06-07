b01ef4c80abcb7f51df53d43e335326e2fbf1ff9
mtraina <matteo.traina.mail@gmail.com>
2015-06-07 19:48:01 +0100

    json added

--- getting-data/project.clj
+++ getting-data/project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [incanter/incanter-core "1.5.6"]
-                 [incanter/incanter-io "1.5.6"]])
+                 [incanter/incanter-io "1.5.6"]
+                 [org.clojure/data.json "0.2.6"]])
