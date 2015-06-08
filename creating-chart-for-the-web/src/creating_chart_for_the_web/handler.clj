ec6ac758052350c6e63dd485d069b3fde1e332f3
mtraina <matteo.traina.mail@gmail.com>
2015-06-08 23:11:00 +0100

    serving json

--- creating-chart-for-the-web/src/creating_chart_for_the_web/handler.clj
+++ creating-chart-for-the-web/src/creating_chart_for_the_web/handler.clj
@@ -1,11 +1,14 @@
 (ns creating-chart-for-the-web.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.util.response :refer [redirect]]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
-  (route/not-found "Not Found"))
+  (GET "/" [] (redirect "/data/census-race.json"))
+  (route/resources "/")
+  (route/not-found "Not Found")
+  )
 
 (def app
   (wrap-defaults app-routes site-defaults))
